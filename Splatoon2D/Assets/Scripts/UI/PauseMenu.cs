454f507418668ecaacb37732878a07e18a112bb1
Delerrr <95750785+Delerrr@users.noreply.github.com>
2022-10-03 14:22:10 +0800

    添加暂停按钮和暂停菜单界面
    点击游戏界面右下角的齿轮按钮即可暂停游戏并打开暂停菜单，暂停菜单有Resume、BackToMainMenu、ExitGame三个按钮。现在游戏终于有退出按钮啦！（终于摆脱Alt+F4了）
    下一步还需为暂停菜单添加选择关卡的按钮。

---
+++ Splatoon2D/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    //空的父对象，存放暂停菜单的三个按钮
+    public GameObject pausemenu;
+    public Button Pause;
+    public Button Resume;
+    public Button BackToMain; 
+    public Button ExitGame; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pause.onClick.AddListener(PauseGame);
+        Resume.onClick.AddListener(ResumeGame);
+        BackToMain.onClick.AddListener(BackToMainMenu);
+        ExitGame.onClick.AddListener(ExitGameFunc);
+    }
+
+    void PauseGame() {
+        Time.timeScale = 0;
+        pausemenu.SetActive(true);
+    }
+
+    void ResumeGame() {
+        Time.timeScale = 1f;
+        pausemenu.SetActive(false);
+    }
+
+    void BackToMainMenu() {
+        //设置鼠标可见
+        Cursor.visible = true;
+        Time.timeScale = 1f;
+        pausemenu.SetActive(false);
+        SceneManager.LoadScene("MainBoard");
+    }
+
+    void ExitGameFunc() {
+#if     UNITY_EDITOR//在编辑器模式下
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();//已发布版本
+#endif
+    }
+}
